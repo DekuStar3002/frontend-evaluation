db54ea4a148b6c8fab922eb541cfc3d9d5e30110
Rahul Behera <rahulbehera2002@gmail.com>
2023-03-01 14:42:51 +0530

    feat: add apiEndPoint

---
+++ src/constants/apiEndPoint.jsx
@@ -0,0 +1,37 @@
+const BACKEND_URL = 'http://localhost:8000/';
+
+const GET_ALL_EVENTS = () => ({
+  baseURL: BACKEND_URL,
+  url: 'api/events',
+  method: 'get'
+});
+
+const GET_EVENT_BY_ID = (id) => ({
+  baseURL: BACKEND_URL,
+  url: `api/events/${id}`,
+  method: 'get'
+});
+
+const UPDATE_EVENT_BY_ID = (id, data) => ({
+  baseURL: BACKEND_URL,
+  url: `api/events/${id}`,
+  method: 'patch',
+  data
+});
+
+const GET_THEMES = () => ({
+  baseURL: BACKEND_URL,
+  url: 'api/themes',
+  method: 'get'
+});
+
+const UPDATE_THEMES = (data) => ({
+  baseURL: BACKEND_URL,
+  url: 'api/themes',
+  method: 'put',
+  data
+});
+
+export default {
+  GET_ALL_EVENTS, GET_EVENT_BY_ID, UPDATE_EVENT_BY_ID, GET_THEMES, UPDATE_THEMES
+};
