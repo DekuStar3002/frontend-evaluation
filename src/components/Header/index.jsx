550f7253ad7e79c017cd93aaba13c8627774e461
Rahul Behera <rahulbehera2002@gmail.com>
2023-03-01 12:24:32 +0530

    feat: add Header Component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <p className="header-title">Eventify</p>
+    </div>
+  );
+}
+
+export default Header;
