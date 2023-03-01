b2c55e97bf86908f7fa3fd81dd82fc46e0243bbf
Rahul Behera <rahulbehera2002@gmail.com>
2023-03-01 17:56:37 +0530

    feat: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
-import { Header, EventCardContainer } from './components';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { EventProvider } from './contexts/EventContext';
+import { AllEventPage, EventPage, ErrorPage, PageNotFound } from './pages';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <EventCardContainer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<EventProvider><AllEventPage /></EventProvider>} />
+          <Route path="/event/:id" element={<EventProvider><EventPage /></EventProvider>} />
+          <Route path="/error/:errorCode" element={<ErrorPage />} />
+          <Route path="/error" element={<ErrorPage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
