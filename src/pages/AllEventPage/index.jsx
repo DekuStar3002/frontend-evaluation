6fbb763f62e332cdce4ed81105a639f4f84684a2
Rahul Behera <rahulbehera2002@gmail.com>
2023-03-01 17:59:05 +0530

    fix: changes in component

---
+++ src/pages/AllEventPage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { EventCardContainer, Header } from '../../components';
+
+function AllEventPage() {
+  return (
+    <div>
+      <Header />
+      <EventCardContainer />
+    </div>
+  );
+}
+
+export default AllEventPage;
