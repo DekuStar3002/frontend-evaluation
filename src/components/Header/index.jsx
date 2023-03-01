39c65f1d4af907b5c9b9f4d802a6ee17546c6cef
Rahul Behera <rahulbehera2002@gmail.com>
2023-03-01 17:58:31 +0530

    fix: add click event

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+  const navigate = useNavigate();
   return (
     <div className="header">
-      <p className="header-title">EVENRIFY</p>
+      <p className="header-title" onClick={() => navigate('/')} style={{'cursor': 'pointed'}}>EVENTIFY</p>
     </div>
   );
 }
